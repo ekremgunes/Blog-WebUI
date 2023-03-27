4afb677ca3bf685963170dc0893c7d7f195125b8
Ekrem Gunes <github1357@gmail.com>
2023-03-27 03:34:41 +0300

    Filters div and hamburger menu added

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+function filterCountry(countryId) {
+    //codes
+    console.log(countryId)
+    var icon = document.querySelector("#worldIcon")
+    icon.style.color = "#0392d8eb"
+    setTimeout(() => {
+        icon.style.color = ""
+    }, 2000);
+}
+function filterTop(params) {
+    //codes
+    console.log("tops")
+}
+function filterHot(params) { //new
+    //codes
+    console.log("hots")
+    
+}
